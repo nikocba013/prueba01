24409f2d72dfd687204cd2ee382ca4ff906fa1ee
Nicolas Ferreyra <nferreyra@v-n.com.ar>
2019-06-03 15:28:20 -0300

    comit1 prueba

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
-    $a = 2;
-    echo $a;
+   
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
